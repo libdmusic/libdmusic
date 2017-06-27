211b0b8517f77b3ed0aed227ba10016aa63a2c54
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2017-06-27 17:47:17 +0200

    Add sampledump utility

---
+++ utils/sampledump/src/sampledump.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdint>
+#include <dmusic/Riff.h>
+#include <dmusic/dls/DownloadableSound.h>
+
+using namespace DirectMusic;
+
+static Riff::Chunk loadChunk(std::string path) {
+    std::ifstream inputStream(path, std::ios::binary | std::ios::ate);
+    if (!inputStream.is_open()) {
+        throw "Couldn't open file";
+    }
+    std::vector<std::uint8_t> buffer(inputStream.tellg());
+    inputStream.seekg(0);
+    inputStream.read((char*)buffer.data(), buffer.size());
+    inputStream.close();
+    Riff::Chunk ch(buffer.data());
+    return ch;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        std::cerr << "Usage: sampledump <inputfile.dls>" << std::endl;
+        return 1;
+    }
+    std::string inputFile = std::string(argv[1]);
+    std::string outputFile = std::string(argv[2]);
+
+    std::cout << "Parsing input file... ";
+    Riff::Chunk chunk = loadChunk(inputFile);
+    std::cout << "Done.\n";
+    std::cout << "Loading DLS structure... ";
+    DLS::DownloadableSound dls(chunk);
+    std::cout << "Done.\n";
+
+    std::cout << "Dumping samples... ";
+    for (const DLS::Wave& wav : dls.getWavePool()) {
+        std::string name = wav.getInfo().getName();
+        std::ofstream ofs(name + ".wav");
+        wav.writeToStream(ofs);
+        ofs.close();
+    }
+    std::cout << "Done: " << dls.getWavePool().size() << " samples dumped.\n";
+    return 0;
+}
