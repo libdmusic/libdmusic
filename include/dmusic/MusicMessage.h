8ef21f74722292cc522e147a589a46db340578aa
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2017-08-15 17:27:32 +0200

    Added messages

---
+++ include/dmusic/MusicMessage.h
@@ -0,0 +1,166 @@
+#pragma once
+#include <cstdint>
+#include "Midi.h"
+
+namespace DirectMusic {
+    enum class MessageType {
+        Tempo,
+        TimeSignature,
+        NoteOn,
+        NoteOff,
+        ChannelPressure,
+        PolyAftertouch,
+        ControlChange,
+        ProgramChange,
+        PitchBend
+    };
+
+    class MusicMessage {
+    public:
+        MusicMessage(std::uint32_t time)
+            : m_messageTime(time) {};
+
+        virtual MessageType getMessageType() const = 0;
+        std::uint32_t getMessageTime() const { return m_messageTime; };
+
+    protected:
+        std::uint32_t m_messageTime;
+    };
+    
+    struct MusicMessageComparer {
+        bool operator()(const MusicMessage& lhs, const MusicMessage& rhs) const {
+            return lhs.getMessageTime() > rhs.getMessageTime();
+        }
+    };
+
+    class NoteOnMessage : public MusicMessage {
+    public:
+        NoteOnMessage(std::uint32_t time, std::uint8_t note, std::uint8_t velocity, std::uint8_t channel)
+            : MusicMessage(time),
+            m_note(note),
+            m_velocity(velocity),
+            m_channel(channel) {};
+
+        std::uint8_t getNote() const { return m_note; }
+        std::uint8_t getVelocity() const { return m_velocity; }
+        std::uint8_t getChannel() const { return m_channel; }
+
+        virtual MessageType getMessageType() {
+            return MessageType::NoteOn;
+        }
+
+    private:
+        std::uint8_t m_note, m_velocity, m_channel;
+    };
+
+    class NoteOffMessage : public MusicMessage {
+    public:
+        NoteOffMessage(std::uint32_t time, std::uint8_t note, std::uint8_t velocity, std::uint8_t channel)
+            : MusicMessage(time),
+            m_note(note),
+            m_velocity(velocity),
+            m_channel(channel) {};
+
+        std::uint8_t getNote() const { return m_note; }
+        std::uint8_t getVelocity() const { return m_velocity; }
+
+        virtual MessageType getMessageType() {
+            return MessageType::NoteOff;
+        }
+
+    private:
+        std::uint8_t m_note, m_velocity, m_channel;
+    };
+
+    class ChannelPressureMessage : public MusicMessage {
+    public:
+        ChannelPressureMessage(std::uint32_t time, std::uint8_t value, std::uint8_t channel)
+            : MusicMessage(time),
+            m_value(value),
+            m_channel(channel) {};
+
+        std::uint8_t getValue() const { return m_value; }
+
+        virtual MessageType getMessageType() {
+            return MessageType::ChannelPressure;
+        }
+
+    private:
+        std::uint8_t m_value, m_channel;
+    };
+
+    class PolyAftertouchMessage : public MusicMessage {
+    public:
+        PolyAftertouchMessage(std::uint32_t time, std::uint8_t note, std::uint8_t value, std::uint8_t channel)
+            : MusicMessage(time),
+            m_note(note),
+            m_value(value),
+            m_channel(channel) {};
+
+        std::uint8_t getNote() const { return m_note; }
+        std::uint8_t getValue() const { return m_value; }
+
+        virtual MessageType getMessageType() {
+            return MessageType::PolyAftertouch;
+        }
+
+    private:
+        std::uint8_t m_value, m_note, m_channel;
+    };
+
+    class ControlChangeMessage : public MusicMessage {
+    public:
+        ControlChangeMessage(std::uint32_t time, DirectMusic::Midi::Control control, std::int32_t value, std::uint8_t channel)
+            : MusicMessage(time),
+            m_control(control),
+            m_value(value),
+            m_channel(channel) {};
+
+        DirectMusic::Midi::Control getControl() const { return m_control; }
+        std::uint8_t getValue() const { return m_value; }
+
+        virtual MessageType getMessageType() {
+            return MessageType::ControlChange;
+        }
+
+    private:
+        DirectMusic::Midi::Control m_control;
+        std::int32_t m_value;
+        std::uint8_t m_channel;
+    };
+
+    class ProgramChangeMessage : public MusicMessage {
+    public:
+        ProgramChangeMessage(std::uint32_t time, std::uint8_t program, std::uint8_t channel)
+            : MusicMessage(time),
+            m_program(program),
+            m_channel(channel) {};
+
+        std::uint8_t getProgram() const { return m_program; }
+
+        virtual MessageType getMessageType() {
+            return MessageType::ProgramChange;
+        }
+
+    private:
+        std::uint8_t m_program, m_channel;
+    };
+
+    class PitchBendMessage : public MusicMessage {
+    public:
+        PitchBendMessage(std::uint32_t time, std::int16_t value, std::uint8_t channel)
+            : MusicMessage(time),
+            m_value(value),
+            m_channel(channel) {};
+
+        std::int16_t getValue() const { return m_value; }
+
+        virtual MessageType getMessageType() {
+            return MessageType::PitchBend;
+        }
+
+    private:
+        std::int16_t m_value;
+        std::uint8_t m_channel;
+    };
+}
--- include/dmusic/PlayingContext.h
+++ include/dmusic/PlayingContext.h
@@ -3,6 +3,8 @@
 #include <cstdint>
 #include <memory>
 #include <map>
+#include <queue>
+#include <mutex>
 #include "Common.h"
 #include "Structs.h"
 #include "InstrumentPlayer.h"
@@ -10,30 +12,60 @@
 #include "Forms.h"
 #include "Loader.h"
 #include "dls/DownloadableSound.h"
+#include "MusicMessage.h"
 
 namespace DirectMusic {
     /// This the main interface to the DirectMusic emulation layer
     template<typename T>
     class PlayingContext {
     private:
+        struct TimeSignature {
+            std::uint8_t beatsPerMeasure;
+            std::uint8_t beat;
+            std::uint16_t subdivisions;
+        };
+
         std::uint32_t m_sampleRate, m_audioChannels;
         const Loader& m_loader;
         std::map<int, std::shared_ptr<InstrumentPlayer>> m_performanceChannels;
+        std::uint32_t m_musicTime;
+        double m_tempo;
+        TimeSignature m_signature;
+        std::mutex m_mutex;
+        std::priority_queue<MusicMessage, std::vector<MusicMessage>, MusicMessageComparer> m_messageQueue;
 
         template<typename T1>
-        std::shared_ptr<T1> genObjFromChunkData(const std::vector<std::uint8_t>& data) const {
+        static std::shared_ptr<T1> genObjFromChunkData(const std::vector<std::uint8_t>& data) {
             if (data.empty()) return nullptr;
             DirectMusic::Riff::Chunk c(data.data());
             return std::make_shared<T1>(c);
         }
 
+        void setTempo(double tempo) {
+            m_mutex.lock();
+            m_tempo = tempo;
+            m_mutex.unlock();
+        }
+
+        void setTimeSignature(std::uint8_t beatsPerMeasure, std::uint8_t beat, std::uint16_t subdivisions) {
+            m_mutex.lock();
+            m_signature = TimeSignature();
+            m_signature.beatsPerMeasure = beatsPerMeasure;
+            m_signature.beat = beat;
+            m_signature.subdivisions = subdivisions;
+            m_mutex.unlock();
+        }
+
     public:
+        static const std::uint32_t PulsesPerQuarterNote = 768;
+
         /// Creates a new playing contest with the specified sampling rate and
         /// number of audio channels (normally 1 (mono) or 2 (stereo))
         PlayingContext(std::uint32_t sampleRate, std::uint32_t audioChannels)
             : m_sampleRate(sampleRate),
             m_audioChannels(audioChannels),
-            m_loader(Loader()) {}
+            m_loader(Loader()),
+            m_musicTime(0) {}
 
         /// Renders the following audio block
         void renderBlock(std::int16_t *data, std::uint32_t count, float volume = 1) noexcept;
@@ -66,5 +98,7 @@ namespace DirectMusic {
             std::vector<std::uint8_t> data = m_loader.loadFile(file);
             return genObjFromChunkData<DirectMusic::DLS::DownloadableSound>(data);
         }
+
+        std::uint32_t getTime() const { return m_musicTime; }
     };
 }
