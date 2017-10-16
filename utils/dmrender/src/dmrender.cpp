ec0080d7dfd5481f16075dbe1cff04768849119e
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2017-10-16 17:45:09 +0200

    We're getting there

--- utils/dmrender/src/dmrender.cpp
+++ utils/dmrender/src/dmrender.cpp
@@ -5,7 +5,10 @@
 #include <dmusic/InstrumentPlayer.h>
 #include <dmusic/Tracks.h>
 #include <dmusic/dls/DownloadableSound.h>
-#include <codecvt>
+#include <cassert>
+#include <fstream>
+#define TSF_IMPLEMENTATION
+#include "tsf.h"
 
 using namespace DirectMusic;
 using namespace DirectMusic::DLS;
@@ -13,27 +16,49 @@ using namespace DirectMusic::DLS;
 static std::queue<std::string> styles;
 
 class MyInstrumentPlayer : public InstrumentPlayer {
+private:
+    tsf* m_soundfont;
+    std::uint32_t bank, preset;
 public:
     MyInstrumentPlayer(std::uint8_t bankLo, std::uint8_t bankHi, std::uint8_t patch,
         const DownloadableSound& dls,
         std::uint32_t sampleRate,
         std::uint32_t channels) : InstrumentPlayer(bankLo, bankHi, patch, dls, sampleRate, channels) {
+        assert(channels == 1);
 
         std::uint32_t bank = (bankHi << 16) + bankLo;
 
         for (const auto& instr : dls.getInstruments()) {
-            if (instr.getMidiBank() == bank && instr.getMidiProgram() == patch) {
+            if (/*instr.getMidiBank() == bank && */instr.getMidiProgram() == patch) {
                 std::cout << "Instrument loaded: " << instr.getInfo().getName() << " from " << dls.getInfo().getName() << "\n";
+                std::string soundfontFile = (dls.getInfo().getName() + ".sf2");
+                m_soundfont = tsf_load_filename(soundfontFile.c_str());
+
+                this->bank = bank;
+                this->preset = patch;
+                // FIXME: Only mono is supported
+                tsf_set_output(m_soundfont, TSF_MONO, sampleRate);
+                return;
             }
         }
+
+        std::cout << "error";
     }
-    virtual std::uint32_t renderBlock(std::int16_t *buffer, std::uint32_t count) noexcept { return count; }
+
+    virtual std::uint32_t renderBlock(std::int16_t *buffer, std::uint32_t count) noexcept {
+        tsf_render_short(m_soundfont, buffer, count, 1);
+        return count;
+    }
 
     /// Instructs the synthesizer to start playing a note
-    virtual void noteOn(std::uint8_t note, std::uint8_t velocity) {}
+    virtual void noteOn(std::uint8_t note, std::uint8_t velocity) {
+        tsf_note_on(m_soundfont, preset, note, velocity / 255.0f);
+    }
 
     /// Instructs the synthesizer to stop playing a note
-    virtual void noteOff(std::uint8_t note, std::uint8_t velocity) {}
+    virtual void noteOff(std::uint8_t note, std::uint8_t velocity) {
+        tsf_note_off(m_soundfont, preset, note);
+    }
 
     /// Sends a "channel pressure" message
     virtual void channelPressure(std::uint8_t val) {}
@@ -257,9 +282,63 @@ void printStyle(const std::shared_ptr<StyleForm> style) {
     }
 }
 
+void writeWave(std::vector<std::int16_t>& wavedata, std::ostream& stream) {
+
+    int totalSize =
+        4 + // RIFF
+        4 + // Total Size
+        4 + // WAVE
+        4 + // FMT
+        4 + // sizeof(WaveFormatEx) + m_extraFmtData.size()
+        sizeof(WaveFormatEx) +
+        4 + // data
+        4 + // m_wavedata.size()
+        wavedata.size() +
+        wavedata.size() % 2 +
+        4 + // fact
+        4 +
+        4;
+
+    std::vector<std::uint8_t> output(totalSize);
+    std::uint8_t *data = output.data();
+
+    memcpy(data, "RIFF", 4); data += 4;
+
+    littleEndianWrite<std::uint32_t>(totalSize - 8, data); data += 4;
+
+    memcpy(data, "WAVE", 4); data += 4;
+
+    memcpy(data, "fmt ", 4); data += 4;
+
+    littleEndianWrite<std::uint32_t>(sizeof(WaveFormatEx), data); data += 4;
+    littleEndianWrite<std::uint16_t>((std::uint16_t)(WaveFormatTag::PCM), data); data += 2;
+    littleEndianWrite<std::uint16_t>(1, data); data += 2;
+    littleEndianWrite<std::uint32_t>(44100, data); data += 4;
+    littleEndianWrite<std::uint32_t>(88200, data); data += 4;
+    littleEndianWrite<std::uint16_t>(2, data); data += 2;
+    littleEndianWrite<std::uint16_t>(16, data); data += 2;
+    littleEndianWrite<std::uint16_t>(0, data); data += 2;
+
+    memcpy(data, "data", 4); data += 4;
+    littleEndianWrite<std::uint32_t>(wavedata.size(), data); data += 4; // Padding byte
+
+    memcpy(data, (const char*)(wavedata.data()), wavedata.size());
+    data += wavedata.size();
+    data += wavedata.size() % 2; // Padding byte
+
+                                   // This chunk is needed by the specs every time
+                                   // the format is not PCM (i.e. FLOAT, ADPCM, whatever...) 
+    memcpy(data, "fact", 4); data += 4;
+    littleEndianWrite<std::uint32_t>(4, data); data += 4;
+    littleEndianWrite<std::uint32_t>(1, data); data += 4;
+
+    stream.write((const char*)output.data(), output.size());
+}
+
+
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        std::cerr << "Usage: dmrender [inputfile]\n";
+    if (argc < 3) {
+        std::cerr << "Usage: dmrender [inputfile] [outputfile]\n";
         return 1;
     }
     PlayingContext ctx(44100, 1, [](std::uint8_t bankLo, std::uint8_t bankHi, std::uint8_t patch,
@@ -288,7 +367,14 @@ int main(int argc, char **argv) {
     }
 
     ctx.playSegment(*segment);
-    ctx.renderBlock(nullptr, 10000000);
+    int sampleRate = 44100;
+    int length = 60 * sampleRate;
+    std::vector<std::int16_t> buffer;
+    buffer.reserve(length);
+    ctx.renderBlock(buffer.data(), length);
+
+    std::ofstream ofs(argv[2], std::ios::binary);
+    writeWave(buffer, ofs);
 
     int a;
     std::cin >> a;
