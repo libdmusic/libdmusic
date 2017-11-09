414dff287c1fd85120ecfcd8b6a2b957f107a8a9
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2017-11-09 12:28:40 +0100

    dls2sf: add linux scripts for generating SF2 files

---
+++ utils/dls2sf/scripts/convert_dls_addonworld.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+if [ "$#" -ne 2 ] || ! [ -d "$1" ];
+then
+  echo "Usage: $0 [G2 directory] [Output file]" >&2
+  exit 1
+fi
+
+MUSICPATH="$1/_work/Data/Music/AddonWorld"
+
+./dls2sf "$MUSICPATH/__DLS_Accordion.dls" \
+  "$MUSICPATH/__DLS_Bass.dls" \
+  "$MUSICPATH/__DLS_Celli.dls" \
+  "$MUSICPATH/__DLS_Fiddle.dls" \
+  "$MUSICPATH/__DLS_Guitar.dls" \
+  "$MUSICPATH/__DLS_Harp.dls" \
+  "$MUSICPATH/__DLS_Horn.dls" \
+  "$MUSICPATH/__DLS_MonoPerc.dls" \
+  "$MUSICPATH/__DLS_RareIngame.dls" \
+  "$MUSICPATH/__DLS_Violins.dls" "$2"
