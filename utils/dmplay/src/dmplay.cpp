f4519dd5665d7760ef4e5ac5ee66f33f3030f8e9
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2017-11-15 20:04:21 +0100

    dmplay and dmrender: remove multiple soundfont support

--- utils/dmplay/src/dmplay.cpp
+++ utils/dmplay/src/dmplay.cpp
@@ -15,129 +15,9 @@
 #include <cmath>
 #include <cstdio>
 #include <args.hxx>
-#define TSF_IMPLEMENTATION
-#include <tsf.h>
 
 using namespace DirectMusic;
 using namespace DirectMusic::DLS;
-
-class MyInstrumentPlayer : public InstrumentPlayer {
-private:
-    int m_preset;
-    tsf* m_soundfont;
-    float m_vol;
-    float m_pan;
-    int m_channels;
-    int m_samplerate;
-    bool m_singleFont;
-    float gainToDecibels(float gain) {
-        return 10 * log10(gain);
-    }
-public:
-    MyInstrumentPlayer(std::map<std::string, tsf*> soundfonts,
-        std::uint8_t bankLo, std::uint8_t bankHi, std::uint8_t patch,
-        const DownloadableSound& dls,
-        std::uint32_t sampleRate,
-        std::uint32_t channels,
-        float volume,
-        float pan)
-        : InstrumentPlayer(bankLo, bankHi, patch, dls, sampleRate, channels, volume, pan)
-        , m_soundfont(nullptr)
-        , m_vol(volume)
-        , m_pan(pan)
-        , m_channels(channels)
-        , m_samplerate(sampleRate)
-        , m_singleFont(false) {
-        assert(channels <= 2);
-        std::uint32_t bank = (bankHi << 16) + bankLo;
-
-        std::string soundfontFile = (dls.getInfo().getName() + ".sf2");
-        if (soundfonts.find(soundfontFile) == soundfonts.end()) {
-            m_soundfont = tsf_load_filename(soundfontFile.c_str());
-            assert(m_soundfont != nullptr);
-            TSFOutputMode outputMode = channels == 1 ? TSF_MONO : TSF_STEREO_INTERLEAVED;
-            tsf_set_output(m_soundfont, outputMode, sampleRate, gainToDecibels(volume) - 3);
-            pan = pan < -1 ? -1 : pan > 1 ? 1 : pan;
-            float volFactorRight = sqrt((pan + 1) / 2);
-            float volFactorLeft = sqrt((-pan + 1) / 2);
-            tsf_set_panning(m_soundfont, volFactorLeft, volFactorRight);
-
-            soundfonts[soundfontFile] = m_soundfont;
-        } else {
-            m_soundfont = soundfonts.at(soundfontFile);
-        }
-        m_preset = tsf_get_presetindex(m_soundfont, 0, patch);
-        assert(m_preset >= 0);
-    }
-
-    MyInstrumentPlayer(tsf* soundfont,
-        std::uint8_t bankLo, std::uint8_t bankHi, std::uint8_t patch,
-        const DownloadableSound& dls,
-        std::uint32_t sampleRate,
-        std::uint32_t channels,
-        float volume,
-        float pan)
-        : InstrumentPlayer(bankLo, bankHi, patch, dls, sampleRate, channels, volume, pan)
-        , m_soundfont(nullptr)
-        , m_vol(volume)
-        , m_channels(channels)
-        , m_singleFont(true) {
-        assert(channels <= 2);
-        std::uint32_t bank = (bankHi << 16) + bankLo;
-
-        m_pan = pan < -1 ? -1 : pan > 1 ? 1 : pan;
-        m_soundfont = soundfont;
-
-        m_preset = tsf_get_presetindex(m_soundfont, 0, patch);
-        assert(m_preset >= 0);
-
-        float volFactorRight = sqrt((m_pan + 1) / 2);
-        float volFactorLeft = sqrt((-m_pan + 1) / 2);
-
-        tsf_set_preset_panning(m_soundfont, m_preset, volFactorLeft, volFactorRight);
-        tsf_set_preset_gain(m_soundfont, m_preset, gainToDecibels(volume));
-    }
-
-    virtual std::uint32_t renderBlock(std::int16_t *buffer, std::uint32_t count, float volume, bool mix) noexcept {
-        if (m_singleFont) {
-            if (!mix) {
-                tsf_render_short(m_soundfont, buffer, count / m_channels, 0);
-            }
-        } else {
-            tsf_render_short(m_soundfont, buffer, count / m_channels, mix ? 1 : 0);
-        }
-        return count;
-    }
-
-    /// Instructs the synthesizer to start playing a note
-    virtual void noteOn(std::uint8_t note, std::uint8_t velocity) {
-        tsf_note_on(m_soundfont, m_preset, note, velocity / 255.0f);
-    }
-
-    /// Instructs the synthesizer to stop playing a note
-    virtual void noteOff(std::uint8_t note, std::uint8_t velocity) {
-        tsf_note_off(m_soundfont, m_preset, note);
-    }
-
-    virtual void allNotesOff() {
-        tsf_all_notes_off(m_soundfont, m_preset);
-    }
-
-    /// Sends a "channel pressure" message
-    virtual void channelPressure(std::uint8_t val) {}
-
-    /// Sends a "polyphonic aftertouch" message to a note
-    virtual void polyAftertouch(std::uint8_t note, std::uint8_t val) {}
-
-    /// Sends a "control change" message
-    virtual void controlChange(DirectMusic::Midi::Control control, std::int32_t val) {}
-
-    /// Sends a "program change" (aka "patch change") message
-    virtual void programChange(std::uint8_t program) {}
-
-    /// Sends a "pitch bend" message
-    virtual void pitchBend(std::int16_t val) {}
-};
 
 static int paCallback(const void *inputBuffer, void *outputBuffer,
     unsigned long framesPerBuffer,
@@ -182,25 +62,20 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    if (!sfont) {
+        std::cerr << "dmplay: No soundfont specified." << std::endl;
+    }
+
     int sampleRate = samplingRate ? args::get(samplingRate) : 44100;
     int channels = numChannels ? args::get(numChannels) : 2;
 
     // Store soundfonts based on their name
     std::map<std::string, tsf*> soundfontMap;
-    std::unique_ptr<PlayingContext> ctx = nullptr;
-
-    if (sfont) {
-        ctx = std::make_unique<PlayingContext>(sampleRate, channels, SoundFontPlayer::createFactory(args::get(sfont)));
-    } else {
-        ctx = std::make_unique<PlayingContext>(sampleRate, channels, [soundfontMap](std::uint8_t bankLo, std::uint8_t bankHi, std::uint8_t patch,
-            const DownloadableSound& dls, std::uint32_t sampleRate, std::uint32_t chans, float vol, float pan) {
-            return std::static_pointer_cast<InstrumentPlayer>(std::make_shared<MyInstrumentPlayer>(soundfontMap, bankLo, bankHi, patch, dls, sampleRate, chans, vol, pan));
-        });
-    }
+    PlayingContext ctx(sampleRate, channels, SoundFontPlayer::createFactory(args::get(sfont)));
     std::cout << "Loading segment...";
-    auto segment = ctx->loadSegment(args::get(segmentName));
+    auto segment = ctx.loadSegment(args::get(segmentName));
     std::cout << " done.\nStart playback... ";
-    ctx->playSegment(*segment);
+    ctx.playSegment(*segment);
     std::cout << " done.\nBegin rendering... ";
 
     PaStream *stream;
@@ -208,7 +83,7 @@ int main(int argc, char **argv) {
     err = Pa_Initialize();
     if (err != paNoError) goto error;
     /* Open an audio I/O stream. */
-    err = Pa_OpenDefaultStream(&stream, 0, channels, paInt16, sampleRate, 256, paCallback, ctx.get());
+    err = Pa_OpenDefaultStream(&stream, 0, channels, paInt16, sampleRate, 256, paCallback, &ctx);
     if (err != paNoError) goto error;
     err = Pa_StartStream(stream);
     if (err != paNoError) goto error;
@@ -220,9 +95,9 @@ int main(int argc, char **argv) {
             break;
         }
         std::cout << "Loading segment...";
-        segment = ctx->loadSegment(input);
+        segment = ctx.loadSegment(input);
         std::cout << " done.\nStart playback... ";
-        ctx->playSegment(*segment);
+        ctx.playSegment(*segment);
         std::cout << " done.\nBegin rendering... ";
     }
     
@@ -231,11 +106,6 @@ int main(int argc, char **argv) {
 
     err = Pa_Terminate();
     if (err != paNoError) goto error;
-
-    // Close all soundfont handles
-    for (const auto& kvp : soundfontMap) {
-        tsf_close(kvp.second);
-    }
 
     std::cout << "Rendering stopped.";
     return 0;
--- utils/dmrender/src/dmrender.cpp
+++ utils/dmrender/src/dmrender.cpp
@@ -3,136 +3,20 @@
 #include <locale>
 #include <map>
 #include <dmusic/PlayingContext.h>
+#ifndef DMUSIC_TSF_SUPPORT
+#define DMUSIC_TSF_SUPPORT 1
+#endif
+#include <dmusic/SoundFontPlayer.h>
 #include <dmusic/InstrumentPlayer.h>
 #include <dmusic/Tracks.h>
 #include <dmusic/dls/DownloadableSound.h>
 #include <cassert>
 #include <sndfile.h>
 #include <cmath>
-#define TSF_IMPLEMENTATION
-#include <tsf.h>
 #include <args.hxx>
 
 using namespace DirectMusic;
 using namespace DirectMusic::DLS;
-
-class MyInstrumentPlayer : public InstrumentPlayer {
-private:
-    int m_preset;
-    tsf* m_soundfont;
-    float m_vol;
-    float m_pan;
-    int m_channels;
-    int m_samplerate;
-    bool m_singleFont;
-    float gainToDecibels(float gain) {
-        return 10 * log10(gain);
-    }
-public:
-    MyInstrumentPlayer(std::map<std::string, tsf*> soundfonts,
-        std::uint8_t bankLo, std::uint8_t bankHi, std::uint8_t patch,
-        const DownloadableSound& dls,
-        std::uint32_t sampleRate,
-        std::uint32_t channels,
-        float volume,
-        float pan)
-        : InstrumentPlayer(bankLo, bankHi, patch, dls, sampleRate, channels, volume, pan)
-        , m_soundfont(nullptr)
-        , m_vol(volume)
-        , m_pan(pan)
-        , m_channels(channels)
-        , m_samplerate(sampleRate)
-        , m_singleFont(false) {
-        assert(channels <= 2);
-        std::uint32_t bank = (bankHi << 16) + bankLo;
-
-        std::string soundfontFile = (dls.getInfo().getName() + ".sf2");
-        if (soundfonts.find(soundfontFile) == soundfonts.end()) {
-            m_soundfont = tsf_load_filename(soundfontFile.c_str());
-            assert(m_soundfont != nullptr);
-            TSFOutputMode outputMode = channels == 1 ? TSF_MONO : TSF_STEREO_INTERLEAVED;
-            tsf_set_output(m_soundfont, outputMode, sampleRate, gainToDecibels(volume) - 3);
-            pan = pan < -1 ? -1 : pan > 1 ? 1 : pan;
-            float volFactorRight = sqrt((pan + 1) / 2);
-            float volFactorLeft = sqrt((-pan + 1) / 2);
-            tsf_set_panning(m_soundfont, volFactorLeft, volFactorRight);
-
-            soundfonts[soundfontFile] = m_soundfont;
-        } else {
-            m_soundfont = soundfonts.at(soundfontFile);
-        }
-        m_preset = tsf_get_presetindex(m_soundfont, 0, patch);
-        assert(m_preset >= 0);
-    }
-
-    MyInstrumentPlayer(tsf* soundfont,
-        std::uint8_t bankLo, std::uint8_t bankHi, std::uint8_t patch,
-        const DownloadableSound& dls,
-        std::uint32_t sampleRate,
-        std::uint32_t channels,
-        float volume,
-        float pan)
-        : InstrumentPlayer(bankLo, bankHi, patch, dls, sampleRate, channels, volume, pan)
-        , m_soundfont(nullptr)
-        , m_vol(volume)
-        , m_channels(channels)
-        , m_singleFont(true) {
-        assert(channels <= 2);
-        std::uint32_t bank = (bankHi << 16) + bankLo;
-
-        m_pan = pan < -1 ? -1 : pan > 1 ? 1 : pan;
-        m_soundfont = soundfont;
-
-        m_preset = tsf_get_presetindex(m_soundfont, 0, patch);
-        assert(m_preset >= 0);
-
-        float volFactorRight = sqrt((m_pan + 1) / 2);
-        float volFactorLeft = sqrt((-m_pan + 1) / 2);
-
-        tsf_set_preset_panning(m_soundfont, m_preset, volFactorLeft, volFactorRight);
-        tsf_set_preset_gain(m_soundfont, m_preset, gainToDecibels(volume));
-    }
-
-    virtual std::uint32_t renderBlock(std::int16_t *buffer, std::uint32_t count, float volume, bool mix) noexcept {
-        if (m_singleFont) {
-            if (!mix) {
-                tsf_render_short(m_soundfont, buffer, count / m_channels, 0);
-            }
-        } else {
-            tsf_render_short(m_soundfont, buffer, count / m_channels, mix ? 1 : 0);
-        }
-        return count;
-    }
-
-    /// Instructs the synthesizer to start playing a note
-    virtual void noteOn(std::uint8_t note, std::uint8_t velocity) {
-        tsf_note_on(m_soundfont, m_preset, note, velocity / 255.0f);
-    }
-
-    /// Instructs the synthesizer to stop playing a note
-    virtual void noteOff(std::uint8_t note, std::uint8_t velocity) {
-        tsf_note_off(m_soundfont, m_preset, note);
-    }
-
-    virtual void allNotesOff() {
-        tsf_all_notes_off(m_soundfont, m_preset);
-    }
-
-    /// Sends a "channel pressure" message
-    virtual void channelPressure(std::uint8_t val) {}
-
-    /// Sends a "polyphonic aftertouch" message to a note
-    virtual void polyAftertouch(std::uint8_t note, std::uint8_t val) {}
-
-    /// Sends a "control change" message
-    virtual void controlChange(DirectMusic::Midi::Control control, std::int32_t val) {}
-
-    /// Sends a "program change" (aka "patch change") message
-    virtual void programChange(std::uint8_t program) {}
-
-    /// Sends a "pitch bend" message
-    virtual void pitchBend(std::int16_t val) {}
-};
 
 int main(int argc, char **argv) {
     args::ArgumentParser parser("dmrender renders DirectMusic segments into audio files");
@@ -170,51 +54,29 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    if (!sfont) {
+        std::cerr << "dmrender: No soundfont specified" << std::endl;
+        return 1;
+    }
+
     int sampleRate = samplingRate ? args::get(samplingRate) : 44100;
     int channels = numChannels ? args::get(numChannels) : 1;
     std::uint64_t length = (chunkLength ? args::get(chunkLength) : 60) * sampleRate;
 
     // Store soundfonts based on their name
-    tsf* soundfont = nullptr;
-    std::map<std::string, tsf*> soundfontMap;
-    std::unique_ptr<PlayingContext> ctx = nullptr;
-
-    if (sfont) {
-        soundfont = tsf_load_filename(args::get(sfont).c_str());
-
-        TSFOutputMode outputMode = channels == 1 ? TSF_MONO : TSF_STEREO_INTERLEAVED;
-        tsf_set_output(soundfont, outputMode, sampleRate, -3);
-
-        ctx = std::make_unique<PlayingContext>(sampleRate, channels, [soundfont](std::uint8_t bankLo, std::uint8_t bankHi, std::uint8_t patch,
-            const DownloadableSound& dls, std::uint32_t sampleRate, std::uint32_t chans, float vol, float pan) {
-            return std::static_pointer_cast<InstrumentPlayer>(std::make_shared<MyInstrumentPlayer>(soundfont, bankLo, bankHi, patch, dls, sampleRate, chans, vol, pan));
-        });
-    } else {
-        ctx = std::make_unique<PlayingContext>(sampleRate, channels, [soundfontMap](std::uint8_t bankLo, std::uint8_t bankHi, std::uint8_t patch,
-            const DownloadableSound& dls, std::uint32_t sampleRate, std::uint32_t chans, float vol, float pan) {
-            return std::static_pointer_cast<InstrumentPlayer>(std::make_shared<MyInstrumentPlayer>(soundfontMap, bankLo, bankHi, patch, dls, sampleRate, chans, vol, pan));
-        });
-    }
+    PlayingContext ctx(sampleRate, channels, SoundFontPlayer::createFactory(args::get(sfont)));
     std::cout << "Loading segment...";
-    auto segment = ctx->loadSegment(args::get(segmentName));
+    auto segment = ctx.loadSegment(args::get(segmentName));
     std::cout << " done.\nStart playback... ";
-    ctx->playSegment(*segment);
+    ctx.playSegment(*segment);
     std::cout << " done.\nBegin rendering... ";
 
     // Each instrument is going to sum its output into the buffer,
     // so we have to start from a blank state, hence calloc
     std::int16_t* buffer = (std::int16_t*)malloc(length * sizeof(std::int16_t));
     for (std::uint64_t i = 0; i < length; i += sampleRate) {
-        ctx->renderBlock(buffer + i, sampleRate);
+        ctx.renderBlock(buffer + i, sampleRate);
         std::cout << ceil((i / (float)length) * 100) << "% ";
-    }
-    
-    // Close all soundfont handles
-    for (const auto& kvp : soundfontMap) {
-        tsf_close(kvp.second);
-    }
-    if (soundfont != nullptr) {
-        tsf_close(soundfont);
     }
 
     SF_INFO info;
