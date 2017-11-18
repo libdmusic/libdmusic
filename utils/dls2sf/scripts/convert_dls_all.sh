e8de4bd5a00d82b7d72f1db7c0a5166e17a6c9d5
markusobi <halbesbyte@googlemail.com>
2017-11-18 21:04:38 +0100

    added bash dls-build script for all worlds with more error handling

---
+++ utils/dls2sf/scripts/convert_dls_all.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ];
+then
+    echo "Usage: $0 [G2 directory]" >&2
+    exit 1
+fi
+
+MUSICPATH="$1/_work/Data/Music"
+DLS2SF="./dls2sf"
+
+if [ ! -d "$1" ]; then
+    >&2 echo "error: specified gothic directory does not exist: $1"
+    exit 1
+fi
+
+if [ ! -d "$MUSICPATH" ]; then
+    >&2 echo "error: the Music directory does not exist under: $MUSICPATH"
+    exit 1
+fi
+
+if [ ! -f "$DLS2SF" ]; then
+    >&2 echo "error: the dls2sf converter executable was not found: $DLS2SF"
+    exit 1
+fi
+
+for worldDirectory in $MUSICPATH/*/; do
+    worldName=$(basename $worldDirectory)
+    echo "Converting *.dls files in directory $worldName"
+    mkdir -p "$worldName"
+    "$DLS2SF" "$worldDirectory"/*.dls "$worldName/Orchestra.sf2"
+done
